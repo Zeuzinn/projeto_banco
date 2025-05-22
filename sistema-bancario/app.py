f296ec4f6b638db049ac53a3731f65c8b22d2171
Eliseu Rodrigues <eliseurodrigues37@gmail.com>
2025-05-22 17:01:36 -0300

    feat: inicializador do programa

---
+++ sistema-bancario/app.py
@@ -0,0 +1,21 @@
+from funcoes import *
+
+saque_diario = 3
+saldo = 0
+extrato = []
+while True:
+    menu()
+    opcao = input('Escolha uma operação: ')
+    print()
+
+    if opcao == '1':
+        saldo = depositar(saldo, extrato)
+    elif opcao == '2':
+        saldo, saque_diario = sacar(saldo, saque_diario, extrato)
+    elif opcao == '3':
+        exibir_extrato(extrato, saldo)
+    elif opcao == '0':
+        print('Encerrando o programa... Até logo!')
+        break
+    else:
+        print('Operação inválida! Escolha outra operação.')
