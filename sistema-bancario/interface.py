b31eb0f68ba5546ed2ea82534b882493b4ed28d9
Eliseu Rodrigues <eliseurodrigues37@gmail.com>
2025-05-30 17:29:01 -0300

    feat: cria classes saque e deposito para realizar transferência

---
+++ sistema-bancario/interface.py
@@ -0,0 +1,23 @@
+from abc import ABC, abstractmethod
+from conta import Conta
+
+class Transacao(ABC):
+    
+    @abstractmethod
+    def registrar(self, conta: Conta):
+        pass
+
+class Deposito(Transacao):
+    def __init__(self, valor: float):
+        self.valor = valor
+
+    def registrar(self, conta: Conta):
+        conta.depositar(self.valor)
+
+class Saque(Transacao):
+    def __init__(self, valor: float):
+        self.valor = valor
+
+    def registrar(self, conta):
+        print(f'Saque efetuado!')
+        conta.sacar(self.valor)
