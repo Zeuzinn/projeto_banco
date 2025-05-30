fdedfa4c6989a22e5a756d8ca9abf859066f10fb
Eliseu Rodrigues <eliseurodrigues37@gmail.com>
2025-05-30 12:21:09 -0300

    feat: modelo base para criar pessoa

---
+++ sistema-bancario/cliente.py
@@ -0,0 +1,19 @@
+from abc import ABC, abstractmethod
+
+class Cliente(ABC):
+    def __init__(self, rua_numero: str, cidade: str, estado: str):
+        self.rua_numero = rua_numero
+        self.cidade = cidade
+        self.estado = estado
+        self.contas = []
+
+    def realizar_transacao(self, conta):
+        ...
+
+    @abstractmethod
+    def adicionar_conta(self, conta): 
+        pass
+    
+    @abstractmethod
+    def mostrar_clientes(self):
+        pass
