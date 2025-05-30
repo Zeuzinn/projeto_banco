fbaee7d1f5de5504704c166bc74ae1453c802dea
Eliseu Rodrigues <eliseurodrigues37@gmail.com>
2025-05-30 17:30:27 -0300

    test: testes para operações bancárias

--- sistema-bancario/app.py
+++ sistema-bancario/app.py
@@ -1,31 +1,42 @@
 from conta_corrente import ContaCorrente
 from pessoa_fisica import PessoaFisica
+from interface import Deposito, Saque
 
 
 pessoa = PessoaFisica('Ingrid Alves', '21-11-2002','12345678911','Antônio Padeiro, nº 15', 'Campo Grande', 'SP')
-pessoa2 = PessoaFisica('Robson de Souza', '07-07-1970','00033344470','Rua 7, nº 70', 'Jalapão', 'TO')
 
-conta = ContaCorrente(pessoa, 100, 1, "0137", 200)
-conta2 = ContaCorrente(pessoa2, 70, 70, "0137", 500)
+pessoa2 = PessoaFisica('Robson de Souza', '17-07-1970','10101070707','Cleiton Rasta, nº 70', 'Monte Verde', 'MG')
 
-# Add Conta dentro da lista de contas
-pessoa.adicionar_conta(conta)
-pessoa2.adicionar_conta(conta2)
-print()
+conta_1 = ContaCorrente(pessoa, 0, 1, "0137", 100)
+conta_2 = ContaCorrente(pessoa2, 0, 2,"0170", 100)
 
-conta.depositar(200)
-conta.sacar(150)
+# Cria uma conta
+pessoa.adicionar_conta(conta_1)
+pessoa2.adicionar_conta(conta_1)
 
-conta2.depositar(700)
-conta2.sacar(350)
+print('-' *50)
+print('=== FAZENDO OPERAÇÕES NA PRÓPRIA CONTA ===')
+print('-'*50)
+# Realiza operações na própria conta
+conta_1.depositar(150)
+conta_1.sacar(75) 
+conta_2.depositar(150)
+conta_2.sacar(50) 
+print(conta_1.saldo())
+print(conta_2.saldo())
 
-pessoas = [pessoa, pessoa2]
-contas = [conta, conta2]
+# ADC valor para transferência
+conta_1_deposito = Deposito(50)
+conta_2_deposito = Deposito(100)
 
 
-# Exibe a lista de clientes
-for pessoa in pessoas:
-    pessoa.mostrar_clientes()
+print('-' *50)
+print('=== FAZENDO TRANSFERÊNCIAS ENTRE CONTAS ===')
+print('-' *50)
+# Realiza transferências para outras contas 
+conta_1_deposito.registrar(conta_2)
+conta_2_deposito.registrar(conta_1)
 
-for conta in contas:
-    conta.exibir_extrato()
+
+print(conta_1.saldo())
+print(conta_2.saldo())
