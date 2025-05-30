99a2ca8b289eac8aec1bfd8171511c6a62cf7d32
Eliseu Rodrigues <eliseurodrigues37@gmail.com>
2025-05-30 17:46:56 -0300

    Correção de bug

--- sistema-bancario/app.py
+++ sistema-bancario/app.py
@@ -4,28 +4,24 @@ from interface import Deposito, Saque
 
 
 pessoa = PessoaFisica('Ingrid Alves', '21-11-2002','12345678911','Antônio Padeiro, nº 15', 'Campo Grande', 'SP')
-
 pessoa2 = PessoaFisica('Robson de Souza', '17-07-1970','10101070707','Cleiton Rasta, nº 70', 'Monte Verde', 'MG')
-
 conta_1 = ContaCorrente(pessoa, 0, 1, "0137", 100)
 conta_2 = ContaCorrente(pessoa2, 0, 2,"0170", 100)
 
 # Cria uma conta
 pessoa.adicionar_conta(conta_1)
-pessoa2.adicionar_conta(conta_1)
+pessoa2.adicionar_conta(conta_2)
 
 print('-' *50)
 print('=== FAZENDO OPERAÇÕES NA PRÓPRIA CONTA ===')
 print('-'*50)
 # Realiza operações na própria conta
 conta_1.depositar(150)
-conta_1.sacar(75) 
+conta_1.sacar(75)
 conta_2.depositar(150)
 conta_2.sacar(50) 
-print(conta_1.saldo())
-print(conta_2.saldo())
 
-# ADC valor para transferência
+# Add valor para transferência
 conta_1_deposito = Deposito(50)
 conta_2_deposito = Deposito(100)
 
